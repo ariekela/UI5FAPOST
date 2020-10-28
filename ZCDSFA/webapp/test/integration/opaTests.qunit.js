5160e49bb1894d856161e472a8786fe06691c2c6
Ariekela Samuel <samuel.a@dickinson-assoc.com>
2020-10-28 15:34:54 +0000

    vERSION1

---
+++ ZCDSFA/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"ns/ZCDSFA/test/integration/AllJourneys"
+	], function () {
+		QUnit.start();
+	});
+});
